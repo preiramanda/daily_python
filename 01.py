b73638048cccc7b31f0d4ade8fac2dbd9a6ae149
preiramanda <preiramanda@gmail.com>
2024-02-21 10:25:21 +0100

    Adding first exercise

---
+++ 01.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city_name = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's the name of your pet?\n")
+
+print("The name of your band could be " + city_name +" "+ pet_name)
---
+++ README.md
@@ -0,0 +1,2 @@
+# london_app_brewery_python
+Python course from the London app brewery bootcamp
