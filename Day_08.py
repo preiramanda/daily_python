b875d2f80408608ce52b30da00328240765c970f
preiramanda <preiramanda@gmail.com>
2024-02-29 12:13:33 +0100

    Caesar Cipher exercise

---
+++ Day_08.py
@@ -0,0 +1,56 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+\n"""
+
+print(logo)
+
+
+
+al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+      'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
+
+while direction != "encode" and direction != "decode":
+    print("Invalid input. Please type 'encode' or 'decode'.")
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+
+text = input("Type your message:\n").lower()
+
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    np = ""
+
+    if direction == "encode":
+        for i in text:
+            position = al.index(i) + shift
+            if position >= len(al):
+                position -= len(al)
+            np += al[position]
+    else:
+        for i in text:
+            position = al.index(i) - shift
+            if position < 0:
+                position += len(al)
+            np += al[position]
+
+    print(f"The Message is: {np}\n")
+
+
+encrypt(text, shift)
