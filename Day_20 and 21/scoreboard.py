a1047a539ab118d7cb989387ba267db6cd3d7995
preiramanda <preiramanda@gmail.com>
2024-03-18 09:30:58 +0100

    Adding v3 Snake Game

--- Day_20 and 21/scoreboard.py
+++ Day_20 and 21/scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -21,6 +22,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt",mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()    
 
